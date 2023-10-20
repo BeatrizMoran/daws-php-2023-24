fab5450e490458f6e7c38a598eeb1b78f8b18382
Beatriz <beatriz.moran@ikasle.egibide.org>
2023-10-20 13:28:40 +0200

    ejer03

--- ejercicios/teoria/ejer01.php
+++ ejercicios/teoria/ejer01.php
@@ -7,14 +7,14 @@
 </head>
 <body>
     <h1>Ejercicio 1</h1>
-    <p>Enunciado: <br> 02. Crea dos variables llamadas nombre y edad. La variable nombre tendrá el
+    <p><b>Enunciado: </b><br> 02. Crea dos variables llamadas nombre y edad. La variable nombre tendrá el
     valor “Mikel” y la variable edad tendrá asignado el valor 22. A continuación crea
     una página que muestre la siguiente frase:</p>
     <?php 
         $nombre = "Mikel";
         $edad = 22;
-
-        echo "Mi amigo $nombre tiene $edad años"
     ?>
+
+    <h3> <?= "Mi amigo $nombre tiene $edad años" ?> </h3>
 </body>
 </html>
