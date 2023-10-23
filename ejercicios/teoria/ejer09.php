a98f1298d08c0a0438eae56f09fc5282a0e900a9
Beatriz <beatriz.moran@ikasle.egibide.org>
2023-10-23 09:21:53 +0200

    ejer9

---
+++ ejercicios/teoria/ejer09.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejer9</title>
+</head>
+<body>
+    <h1>Ejercicio 8</h1>
+    <p><b>Enunciado: </b><br>.Escribe una función que reciba como parámetro dos cadenas de texto y
+devuelva la concatenación de dichas cadenas. Muestra el resultado obtenido
+por pantalla</p>
+   
+<?php
+   $c1 = $_GET["c1"];
+   $c2 = $_GET["c2"];
+
+   function combinar($c1, $c2){
+    return $c1 . $c2;
+   }
+
+   echo "La concatenacion es: " . combinar($c1, $c2);
+  
+?>
+
+</body>
+</html>
