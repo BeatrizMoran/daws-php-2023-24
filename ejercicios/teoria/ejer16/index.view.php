9673f90a53e27393bb89def97218a2e7be8bd2fa
Beatriz <beatriz.moran@ikasle.egibide.org>
2023-10-23 12:44:40 +0200

    ejer16

---
+++ ejercicios/teoria/ejer16/index.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejer16</title>
+</head>
+<body>
+    <h1>Ejercicio 16</h1>
+    <p><b>Enunciado: </b><br>Crea una función que reciba 2 números obtenidos mediante GET. La aplicación
+deberá realizar la siguiente operación:<br>
+● Si los números son distintos, mostrará el resultado de su suma.<br>
+● Si los números son iguales, mostrará el resultado de su multiplicación.</p>
+   
+
+<p><?= "Resultado: " . $resultado ?></p>
+
+</body>
+</html>
