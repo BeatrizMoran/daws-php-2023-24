f503fdeae44cf2256a244f1eccfab8a0157e5150
Beatriz <beatriz.moran@ikasle.egibide.org>
2023-10-23 13:37:43 +0200

    ejer18

---
+++ ejercicios/teoria/ejer18/index.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejer15</title>
+</head>
+<body>
+    <h1>Ejercicio 17</h1>
+    <p><b>Enunciado: </b><br>Crea una función que reciba un número indicando el día de la semana y que
+muestre por pantalla el día de la semana. En caso de recibir otro número que no
+esté entre el 1 y el 7, mostrará el mensaje “No es ningún día de la semana". Utiliza
+un switch para realizar el ejercicio.<br>
+diaSemana(2) → “El día número 2 es martes.”<br>
+diaSemana(9) → “No es ningún día de la semana”</p>
+   
+
+<h3><?= $dia ?></h3>
+
+</body>
+</html>
