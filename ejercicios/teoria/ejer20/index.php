95aa5c3046af40e62b0899ad0ea2e6b3385cb727
Beatriz <beatriz.moran@ikasle.egibide.org>
2023-10-23 13:44:04 +0200

    ejer20

---
+++ ejercicios/teoria/ejer20/index.php
@@ -0,0 +1,14 @@
+<?php
+
+function sumar($n){
+    $suma = 0;
+    for ($i=0; $i <= $n ; $i++) { 
+        if($i%2 === 0)
+            $suma = $suma + $i;
+    }
+    return $suma;
+}
+
+$resultado = sumar(10);
+
+include "index.view.php";
