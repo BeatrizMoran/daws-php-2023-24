ef0b9179c30d4543afddeb52c310ac58b7a7ff5d
Beatriz <beatriz.moran@ikasle.egibide.org>
2023-10-25 12:39:55 +0200

    ejer 24 y 25

---
+++ ejercicios/teoria/ejer24/index.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejer24</title>
+</head>
+<body>
+    <h1>Ejercicio 24</h1>
+    <p><b>Enunciado: </b><br>Crea un array multidimensional que simule una agenda de contactos (contendrá de cada
+persona su nombre, apellidos, teléfono y dirección de correo electrónico). Implementa una
+función que imprima en una tabla los datos de las personas utilizando la sentencia for.<br>
+Nota: Puedes utilizar la función count() para conocer el tamaño del array que debes recorrer.
+<br>
+   
+
+<?= $tablaFinal;?>
+
+</body>
+</html>
---
+++ ejercicios/teoria/ejer25/index.view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejer24</title>
+</head>
+<body>
+    <h1>Ejercicio 25</h1>
+    <p><b>Enunciado: </b><br>Crea un array con un listado de estudiantes (Ane, Markel, Nora, Danel, Amaia, Izaro). A
+continuación recorre el array utilizando una estructura de control WHILE generando una lista 
+<br>
+   
+
+<h3><?= $prueba?></h3>
+
+</body>
+</html>
