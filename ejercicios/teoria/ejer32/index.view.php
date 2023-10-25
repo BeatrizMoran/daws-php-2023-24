e8f15cad920daee8266bbc2f530835e9f4eb3907
beatriz <beatriz.moran@ikasle.egibide.org>
2023-10-25 20:05:40 +0200

    terminado

--- ejercicios/teoria/ejer32/index.view.php
+++ ejercicios/teoria/ejer32/index.view.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejer29</title>
+</head>
+<body>
+    <h1>Ejercicio 32</h1>
+    <p><b>Enunciado: </b><br>Crea un array asociativo que almacene los nombres de 4 estudiantes y las
+notas de dos exámenes. A continuación, crea de forma dinámica una tabla
+como la de la imagen utilizando los valores del array y la estructura de
+repetición que creas conveniente. Cuando la nota sea inferior a 5, esta deberá
+mostrarse en rojo <p>
+<br>
+
 <table border="1">
     <tr>
         <th>Nombre</th>
@@ -20,3 +36,8 @@
             </tr>
      <?php }?>
 </table>
+
+
+
+</body>
+</html>
