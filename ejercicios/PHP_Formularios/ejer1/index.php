054315c3d53212ced3ffaac9b9f13d6bb6a0963e
Beatriz <beatriz.moran@ikasle.egibide.org>
2023-10-27 12:43:44 +0200

    form1

---
+++ ejercicios/PHP_Formularios/ejer1/index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+   $grados = $_POST["temp"];
+   $prueba = $_POST["c"];
+
+
+   function convertirTemp($grados, $prueba){
+      if($grados != null){
+         if($prueba === 'cel'){
+            return ($grados * (9/5) + 32);
+         }else{
+            return ($grados -32) * (5/9);
+
+         }
+      }
+      else{
+         echo '<script>alert("Error.- Introduce una temperatura.");</script>';
+      }
+
+   }
+
+   $temp = round(convertirTemp($grados,$prueba),2);
+
+   require "index.view.php";
+
+  
+?>
---
+++ ejercicios/PHP_Formularios/ejer1/index.view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejer10</title>
+</head>
+<body>
+    <h1>Ejercicio 12</h1>
+    <p><b>Enunciado: </b><br>Crea un convertidor de grados Celsius a Farenheit (y viceversa). La aplicación
+pedirá una cantidad al usuario y la unidad mediante un formulario. Una vez enviado,
+mostrará el resultado de la conversión realizada. El formulario siempre se mostrará,
+de forma que el usuario pueda seguir introduciendo valores. </p>
+   
+<h3><?=  "Resultado de la conversion ($grados $prueba): $temp"; ?></h3>
+
+<form action="index.php" method="post">
+    <label>Introduce la temperatura: </label>
+    <input type="text" name="temp" id="temp"><br>
+    <label>Indica la unidad de la temperatura introducida: </label>
+    <select  name="c">
+        <option value="cel" >Celsius</option>
+        <option value="far">Farenheit</option>
+
+    </select><br><br>
+    <input type="submit" value="Enviar">
+</form>
+
+</body>
+</html>
